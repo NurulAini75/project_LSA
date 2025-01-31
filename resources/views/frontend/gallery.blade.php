699f136f4191eacc7298f9ccdbcaf728dc5352bb
Nurul Aini <epotetoe@gmail.com>
2025-01-31 17:33:48 +0800

    gallery

--- resources/views/frontend/gallery.blade.php
+++ resources/views/frontend/gallery.blade.php
@@ -1,18 +1,10 @@
-<div id="gallery" class="bg-green_lsa-50">
+<div id="gallery" class="bg-green_lsa-50 p-6">
     <div>
         <h1 class="text-center text-green_lsa-300 text-3xl font-semibold">Gallery</h1>
     </div>
-    <div class="flex flex-row mt-10">
-        {{-- Forech Disini --}}
-        <img src="{{ asset('asset/Foto 8.png') }}" class="h-70 w-72 mr-3">
-        <img src="{{ asset('asset/Foto 1.png') }}" class="h-70 w-72 mr-3">
-        <img src="{{ asset('asset/Foto 7.png') }}" class="h-70 w-72 mr-3">
-        <img src="{{ asset('asset/Foto 2.png') }}" class="h-70 w-72 mr-3">
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
+        @foreach ($gallery as $item)
+            <img src="{{ asset($item->gambar) }}" class="h-70 w-full object-cover rounded-lg">
+        @endforeach
     </div>
-    {{-- <div class="flex flex-row-reverse mt-3">
-        <img src="{{ asset('asset/Foto 6.png') }}" class="h-70 w-72 ml-3">
-        <img src="{{ asset('asset/Foto 4.png') }}" class="h-70 w-72 ml-3">
-        <img src="{{ asset('asset/Foto 3.png') }}" class="h-70 w-72 ml-3">
-        <img src="{{ asset('asset/Foto 5.png') }}" class="h-70 w-72 ml-3">
-    </div> --}}
 </div>
